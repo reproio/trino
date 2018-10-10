3f1bc8bb0f9512504fcea01a3ede914ef7af8860
abicky <takeshi.arabiki@gmail.com>
2018-10-10 23:45:10 +0900

    Introduce cassandra.skip-parition-check
    NativeCassandraSession#getPartitions is very slow
    if there are many partitions used in WHERE clause.
    In our use, the partitions usually exist, so
    introduce cassandra.skip-parition-check property
    to reduce planning time.

--- plugin/trino-cassandra/src/main/java/io/trino/plugin/cassandra/CassandraClientModule.java
+++ plugin/trino-cassandra/src/main/java/io/trino/plugin/cassandra/CassandraClientModule.java
@@ -185,7 +185,8 @@ public class CassandraClientModule
                     CassandraTelemetry cassandraTelemetry = CassandraTelemetry.create(openTelemetry);
                     return cassandraTelemetry.wrap(cqlSessionBuilder.build());
                 },
-                config.getNoHostAvailableRetryTimeout());
+                config.getNoHostAvailableRetryTimeout(),
+                config.isSkipPartitionCheck());
     }
 
     private static Optional<SSLContext> buildSslContext(
